98bb09fba19ac04d77de73c1ffbc79bcef2aaf83
Lenard Frommelt <lenny09918050@gmail.com>
2022-03-21 11:40:16 +0100

    added basic source

---
+++ custom-packages-updater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/builder/builds/custom-package-script/
+uptodate=$(git pull origin master | gawk '{if($0=="Already up to date.") print "yes"}')
+if [$updtodate == "yes"]
+then
+    echo "nothing to do."
+else
+    echo "do update."
+    ./create-packages.sh
+    cd /home/builder/repo-packages/custom-packages/
+    repo-add custom-packages.db.tar.xz *.pkg.tar.xz -R -s
+    echo "done."
+fi
